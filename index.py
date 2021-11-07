78c2680a071b7ec3880b83b130c2588c53246210
Alaqmaar <thewardstricks@outlook.com>
2021-11-07 22:42:32 +0400

    Completed Alg

--- index.py
+++ index.py
@@ -1,10 +1,8 @@
-# Team Registration
-
 nTeams = int(input('Enter the number of participating teams: '))
 teams = []
 elo = []
-round = []
-eround = []
+pairs = []
+epairs = []
 
 print()
 
@@ -14,8 +12,8 @@ for i in range(0, nTeams):
     print()
 
 if len(teams) % 2 != 0:
-    round += [teams[elo.index(max(elo))]]
-    eround += [max(elo)]
+    pairs += [teams[elo.index(max(elo))]]
+    epairs += [max(elo)]
 
     print(max(elo))
     teams.remove(teams[elo.index(max(elo))])
@@ -24,37 +22,39 @@ if len(teams) % 2 != 0:
 i = 1
 
 while len(teams) > 1:
-    t1 = teams[elo.index(max(elo))]
-    e1 = max(elo)
+    while len(teams) > 1:
+        t1 = teams[elo.index(max(elo))]
+        e1 = max(elo)
 
-    teams.remove(t1)
-    elo.remove(max(elo))
+        teams.remove(t1)
+        elo.remove(max(elo))
 
-    t2 = teams[elo.index(max(elo))]
-    e2 = max(elo)
+        t2 = teams[elo.index(max(elo))]
+        e2 = max(elo)
 
-    teams.remove(t2)
-    elo.remove(max(elo))
+        teams.remove(t2)
+        elo.remove(max(elo))
 
-    print('Match', str(i) + ':', t1, 'vs', t2)
+        print('Match', str(i) + ':', t1, 'vs', t2)
 
-    i += 1
+        i += 1
 
-    win = input('Enter the name of the winning team: ')
+        win = input('Enter the name of the winning team: ')
 
-    if t1.startswith(win):
-        round += [t1]
-        eround += [e1]
-    else:
-        round += [t2]
-        eround += [e2]
+        if t1.startswith(win):
+            pairs += [t1]
+            epairs += [e1]
+        else:
+            pairs += [t2]
+            epairs += [e2]
 
-print(round)
-print(eround)
+    print(pairs)
+    print(epairs)
 
-# Winning criteria
+    teams = pairs
+    elo = epairs
 
 print()
 
-if len(teams) == 1:
+if len(epairs) == 1:
     print(teams[0], 'has won the tournament!')
