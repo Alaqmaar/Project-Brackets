b268a659b132d826b5b448d9f7c812f873ce8107
Alaqmaar <thewardstricks@outlook.com>
2021-11-07 18:00:46 +0400

    Team pairings for first round Odd number of teams Winning Condition

--- index.py
+++ index.py
@@ -1,29 +1,60 @@
-# Project Brackets
-# Create a Tournament Management System
+# Team Registration
 
-# First team to 13 points wins the game
+nTeams = int(input('Enter the number of participating teams: '))
+teams = []
+elo = []
+round = []
+eround = []
 
-a = 0
-b = 0
+print()
 
-print('Type \'A\' to add a point to Team A.')
-print('Type \'B\' to add a point to Team B.')
+for i in range(0, nTeams):
+    teams += [input('Enter the name of Team: ')]
+    elo += [int(input('Enter the teams elo (If the team is not rated, enter 0): '))]
+    print()
 
-while a < 13 and b < 13:
-    pt = input()
+if len(teams) % 2 != 0:
+    round += [teams[elo.index(max(elo))]]
+    eround += [max(elo)]
 
-    if pt == 'A':
-        a += 1
-    elif pt == 'B':
-        b += 1
+    print(max(elo))
+    teams.remove(teams[elo.index(max(elo))])
+    elo.remove(max(elo))
+
+i = 1
+
+while len(teams) > 1:
+    t1 = teams[elo.index(max(elo))]
+    e1 = max(elo)
+
+    teams.remove(t1)
+    elo.remove(max(elo))
+
+    t2 = teams[elo.index(max(elo))]
+    e2 = max(elo)
+
+    teams.remove(t2)
+    elo.remove(max(elo))
+
+    print('Match', str(i) + ':', t1, 'vs', t2)
+
+    i += 1
+
+    win = input('Enter the name of the winning team: ')
+
+    if t1.startswith(win):
+        round += [t1]
+        eround += [e1]
     else:
-        print('Invalid Input')
-        continue
+        round += [t2]
+        eround += [e2]
 
-    print('Team A:', a)
-    print('Team B:', b)
+print(round)
+print(eround)
 
-if a == 13:
-    print('Team A Wins!')
-else:
-    print('Team B Wins!')
+# Winning criteria
+
+print()
+
+if len(teams) == 1:
+    print(teams[0], 'has won the tournament!')
